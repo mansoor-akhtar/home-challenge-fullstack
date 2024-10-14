729dfbb2c3da846e9fd29ec2afef21c11b4b2885
Mansoor <mansoor_akhtar6@yahoo.com>
2024-10-14 15:43:17 +0500

    schdedule and import from news third party instructions added

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('fetch:news-api')->daily();
+Schedule::command('fetch:new-york-times-api')->daily();
+Schedule::command('fetch:the-guardian-api')->daily();
